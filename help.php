20ca580bd3a35ba36781046c6dcfe33f208690ad
faisal-abdelnase <125914699+faisal-abdelnase@users.noreply.github.com>
2023-05-07 17:54:11 +0300

    Add files via upload

---
+++ help.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help</title>
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+<body>
+<div class = "logo">
+        <img class = "logoimage" src="../image/ATM1.jpg" alt="">
+        <p class = "bankname">Bank Faisal</p>
+    </div>
+
+    <div>
+        <img class = "header" src="../image/ATM3.gif">
+    </div>
+
+    <div class="sec">
+    <pre>
+    <img class="imag" src = "../image/ATM4.gif" alt="">
+        1.Likes to log in by writing the password and email
+        2. You must ensure that the password and email address
+        are correct
+    </pre>
+    </div>
+
+    <div class="sec">
+    <pre>
+    <img class="imag" src = "../image/ATM5.gif" alt="">
+        1.Enter the account PIN that you want to make transactions  
+        2. If There is an error in the pin please go to nearby 
+        bank branch
+    </pre>
+    </div>
+
+    <div class="sec">
+    <pre>
+    <img class="imag" src = "../image/ATM8.gif" alt="">
+        1. Select the transactions you want to perform
+        2. if the transaction is a withdrawal, please
+        enter the amount you want to withdraw
+    </pre>
+    </div>
+
+    <div class="sec">
+    <pre>
+    <img class="imag" src = "../image/ATM6.jpeg" alt="">
+        1. if the transaction is a deposit, please
+        enter the amount you want to deposit
+        2. Please enter the amount you want to deposit
+        in the ATM
+    </pre>
+    </div>
+
+    <div>
+        <a href="index.php"><button class="back" type="button">Back</button></i></a>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ATM</title>
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class = "logo">
+        <img class = "logoimage" src="../image/ATM1.jpg" alt="">
+        <p class = "bankname">Bank Faisal</p>
+    </div>
+
+    <div>
+    <img class = img src="../image/ATM2.jpg" alt="">
+        <pre class = text>
+            <span class = addr>Welcome To ATM Services</span>
+
+            We provide you with all required services
+            to deposit and withdraw your money safely
+        </pre>
+    </div>
+
+    <div class = log>
+        <a href="logein"><button class="but1" type="button">Loge in</button></a>
+        <a href="help.php"><button class="but2" type="button">Help</button></a>
+    </div>
+
+    
+    
+</body>
+</html>
